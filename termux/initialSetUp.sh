c467d7eacfc3f96bb41cc2d5ccbd637e44c2bbf8
tttt369 <tadokoro123@outlook.com>
2025-07-16 21:38:52 +0900

    Update initialSetUp.sh

--- termux/initialSetUp.sh
+++ termux/initialSetUp.sh
@@ -7,7 +7,7 @@ pkg update
 termux-setup-storage
 
 # proot-distroのインストール
-pkg install -y proot-distro
+pkg install -y proot-distro neovim
 
 # Arch Linuxのインストール
 proot-distro install archlinux
@@ -16,5 +16,8 @@ proot-distro install archlinux
 proot-distro login archlinux --bind ~/storage:/mnt
 
 # Ansibleプレイブックの実行
-ansible-playbook -i ~/ansible/termux/hosts ~/ansible/termux/playbook.yml
+pacman -S git neovim
 
+git clone https://github.com/tttt369/ansible.git
+
+ansible-playbook -i ~/ansible/termux/hosts ~/ansible/termux/playbook.yml
