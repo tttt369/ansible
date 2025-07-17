4496e456785e673b365a28af6c7ec32792c0935d
desktop <you@example.com>
2025-07-17 18:47:52 +0900

    update bash

--- termux/secondSetUp.sh
+++ termux/secondSetUp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 pacman -Syu
-pacman -S ansible neovim
-proot-distro login archlinux --bind ~/storage:/mnt
+pacman -S ansible neovim sudo
 ansible-playbook -i /mnt/ansible/termux/hosts /mnt/ansible/termux/playbook.yml
