ed1568aff1bf95fb048139fd4069de5a8935155d
desktop <you@example.com>
2025-07-23 17:07:12 +0900

    add floorp settings

---
+++ dot-config/floorp/user.js
@@ -0,0 +1,15 @@
+user_pref("floorp.panelSidebar.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("floorp.design.configs", "{\"globalConfigs\":{\"faviconColor\":false,\"userInterface\":\"lepton\",\"appliedUserJs\":\"\"},\"tabbar\":{\"tabbarStyle\":\"horizontal\",\"tabbarPosition\":\"optimise-to-vertical-tabbar\",\"multiRowTabBar\":{\"maxRowEnabled\":false,\"maxRow\":3}},\"tab\":{\"tabScroll\":{\"enabled\":false,\"reverse\":false,\"wrap\":false},\"tabMinHeight\":30,\"tabMinWidth\":76,\"tabPinTitle\":false,\"tabDubleClickToClose\":false,\"tabOpenPosition\":-1},\"uiCustomization\":{\"navbar\":{\"position\":\"top\",\"searchBarTop\":false},\"display\":{\"disableFullscreenNotification\":false,\"deleteBrowserBorder\":false,\"hideUnifiedExtensionsButton\":false},\"special\":{\"optimizeForTreeStyleTab\":false,\"hideForwardBackwardButton\":false,\"stgLikeWorkspaces\":false},\"multirowTab\":{\"newtabInsideEnabled\":false},\"bookmarkBar\":{\"focusExpand\":false},\"disableFloorpStart\":true}}"); // show close maximize minimize buttons
+user_pref("browser.download.useDownloadDir", false);
+user_pref("signon.management.page.breach-alerts.enabled", false);
+user_pref("signon.rememberSignons", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("floorp.mousegesture.enabled", false);
+user_pref("floorp.workspaces.enabled", false);
+user_pref("floorp.workspaces.v4.config", "{\"manageOnBms\":false,\"showWorkspaceNameOnToolbar\":false,\"closePopupAfterClick\":false}");
+user_pref("floorp.browser.ssb.config", "{\"showToolbar\":false}");
+user_pref("floorp.browser.ssb.enabled", false);
+user_pref("floorp.browser.deletedWidgets", "[\"fxa-toolbar-menu-button\"]");
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"undo-closed-tab\",\"customizableui-special-spring1\",\"vertical-spacer\",\"urlbar-container\",\"customizableui-special-spring2\",\"downloads-button\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"vertical-tabs\":[],\"PersonalToolbar\":[\"personal-bookmarks\"],\"nora-statusbar\":[\"screenshot-button\",\"fullscreen-button\",\"status-text\"]},\"seen\":[\"undo-closed-tab\",\"developer-button\",\"screenshot-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"vertical-tabs\",\"nora-statusbar\"],\"currentVersion\":23,\"newElementCount\":2}"); // Disable account icon
+
