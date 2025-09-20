9d4adccce85727a3ddd01cacbcb20cfa54d7a0c8
desktop <you@example.com>
2025-09-21 08:06:11 +0900

    changed

--- termux/initialSetUp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pkg update
-termux-setup-storage
-pkg install -y proot-distro vim termux-api
-proot-distro install archlinux
-cp -r ~/ansible ~/storage
-proot-distro login archlinux --bind ~/storage:/mnt
--- termux/secondSetUp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pacman -Syu
-pacman -S ansible neovim sudo
-ansible-playbook -i /mnt/ansible/termux/hosts /mnt/ansible/termux/playbook.yml
