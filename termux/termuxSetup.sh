ea0f22cc4b2b22eeaf09224468e08d3a2b819eb4
desktop <you@example.com>
2025-09-21 08:26:37 +0900

    change

--- termux/termuxSetup.sh
+++ termux/termuxSetup.sh
@@ -2,7 +2,7 @@
 
 pkg update
 termux-setup-storage
-pkg install -y proot-distro vim termux-api
+pkg install -y proot-distro vim termux-api sudo
 proot-distro install archlinux
 cp -r ~/ansible ~/storage
 proot-distro login archlinux --bind ~/storage:/mnt
