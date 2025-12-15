48a8d255598f58219978537ba8073de7344bffd8
desktop <you@example.com>
2025-12-15 09:01:07 +0900

    change

--- dot-config/nvim/lua/plugins/local/mini-toggleterm/lua/mini-toggleterm/init.lua
+++ dot-config/nvim/lua/plugins/local/mini-toggleterm/lua/mini-toggleterm/init.lua
@@ -1,6 +1,5 @@
 local M = {}
 M.file2term = {}
-M.terminal_buf = {}
 M.height = 20
 
 local function debug_obj(obj) 
@@ -19,36 +18,33 @@ local function open_terminal()
   print("M.file2term")
   M.file2term[file_buf] = term_buf
   print(debug_obj(M.file2term))
-
-  print("M.terminal_buf")
-  table.insert(M.terminal_buf, term_buf)
-  print(debug_obj(M.terminal_buf))
 end
 
 local function test()
 end
 
-
 local function toggle_terminal()
   local current_tab = vim.api.nvim_get_current_tabpage()
-  local win_list = vim.api.nvim_tabpage_list_wins(current_tab)
+  local win_list = vim.api.nvim_tabpage_list_wins(current_tab) -- Get all windows from current tab.
 
+  -- Convert the currently displayed buff_id to a boolean element.
   local win_obj_buf = {}
-  local win_list_buf = {}
-  for _, win in ipairs(win_list) do
-    buf = vim.api.nvim_win_get_buf(win)
-    table.insert(win_list_buf, buf)
+  for _, win in pairs(win_list) do
+    local buf = vim.api.nvim_win_get_buf(win)
     win_obj_buf[buf] = true
   end
 
   print("win_obj_buf")
   print(debug_obj(win_obj_buf))
 
+  -- Check all terminal window in current tabs.
   local term_buf
-  for _, buf in ipairs(M.terminal_buf) do
-    if (#M.file2term == 0) then break end
+  for _, buf in pairs(M.file2term) do
+    if (#M.file2term == 0) then 
+      print("break no term")
+      break 
+    end
     print("loop_buf1", buf)
-    print(debug_obj(M.terminal_buf))
     if (win_obj_buf[buf]) then
       print("term_buf", buf)
       term_buf = buf
@@ -56,8 +52,9 @@ local function toggle_terminal()
     end
   end
 
+  -- Find the window thats initiate the terminal window.
   local past_term_buf
-  for _, id in ipairs(win_list_buf) do
+  for buf, _ in pairs(win_obj_buf) do
     if (#M.file2term == 0) then 
       print("break no term")
       break
@@ -75,13 +72,14 @@ local function toggle_terminal()
     end
   end
 
+  -- If there is terminal window.
   if (term_buf) then
     print("if")
     for k,v in pairs(vim.fn.win_findbuf(term_buf)) do 
       M.height = vim.api.nvim_win_get_height(v)
       vim.api.nvim_win_hide(v)
     end 
-
+  -- If there is window thats initiate the terminal.
   elseif (past_term_buf) then
     print("elseif")
     vim.cmd('botright ' .. M.height .. 'split | buffer ' .. past_term_buf)
